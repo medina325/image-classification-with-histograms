0f9bbac03b1dad6f83fd809dba2a61770e8fd1e5
Gabriel Medina Braga <56964034+medina325@users.noreply.github.com>
2021-09-03 23:01:20 -0400

    Mudando estrutura das imagens

--- image_recovery.py
+++ image_recovery.py
@@ -4,8 +4,61 @@ import os
 from zipfile import ZipFile
 import re
 import matplotlib.pyplot as plt
+import sys
 
-def get_images():
+# Every image is gonna be represented by a Image class
+class Image:
+  filename = ''
+  contents = []
+  histograms = {
+    "red": np.zeros(256, np.uint),
+    "green": np.zeros(256, np.uint),
+    "blue": np.zeros(256, np.uint)
+  }
+  pdf = {
+    "red": np.zeros(256, np.uint),
+    "green": np.zeros(256, np.uint),
+    "blue": np.zeros(256, np.uint)
+  }
+
+  def __init__(self, filename: str, contents: list) -> None:
+      self.filename = filename
+      self.contents = np.array(contents, np.uint8)
+
+      self.create_histogram()
+      self.create_pdfs()
+
+  """
+    Initialize Image's histograms
+  """
+  def create_histogram(self):
+    histogram_r = np.zeros(256, np.uint)
+    histogram_g = np.zeros(256, np.uint)
+    histogram_b = np.zeros(256, np.uint)
+    
+    w, h, _ = self.contents.shape
+
+    for x in range(w):
+      for y in range(h):
+        histogram_r[self.contents[x][y][0]] += 1
+        histogram_g[self.contents[x][y][1]] += 1
+        histogram_b[self.contents[x][y][2]] += 1
+
+    self.histograms['red'] = histogram_r
+    self.histograms['green'] = histogram_g
+    self.histograms['blue'] = histogram_b
+
+  def create_pdfs(self):
+    histogram_length = self.histogram['red'].shape[0]
+    
+    self.pdf['red'] = self.histogram['red'] / histogram_length
+    self.pdf['green'] = self.histogram['green'] / histogram_length
+    self.pdf['blue'] = self.histogram['blue'] / histogram_length
+
+# --------------------------------------------------------------------------------------------
+def initialize_images():
+  # Abra o zip e coloque as imagens em uma lista
+
   if (not os.path.exists('Vistex')):
     with ZipFile('Vistex.zip', 'r') as zip:
       for filename in zip.namelist():
@@ -14,39 +67,13 @@ def get_images():
     zip.close()
 
   files = os.listdir('Vistex')
-  # imgs = [io.imread(f'Vistex/{file}') for file in files]
-  imgs = [{'filename': file, 'contents': io.imread(f'Vistex/{file}')} for file in files]
-  
-  return imgs
 
-"""
-  Returns dictionary relating images to it's histograms
-"""
-def make_histograms(imgs):
-  imgs_w_histograms = []
+  # imgs = []
+  # for i in range(0,3):
+  #   imgs.append(Image(files[i], io.imread(f'Vistex/{files[i]}')))
+  # return imgs
 
-  for img in imgs[:2]:
-    print("oi")
-    histogram_r = histogram_g = histogram_b = np.zeros(256, np.uint)
-    w, h, _ = img['contents'].shape
-    img_np = np.array(img['contents'], np.uint8)
-    
-    for x in range(w):
-      for y in range(h):
-        histogram_r[img_np[x][y][0]] += 1
-        histogram_g[img_np[x][y][1]] += 1
-        histogram_b[img_np[x][y][2]] += 1
-
-    imgs_w_histograms.append({
-      **img,
-      'histogram': {
-        'red': histogram_r,
-        'green': histogram_g,
-        'blue': histogram_b
-      }
-    })
-    
-  plot_histogram(histogram_r, 'r')
+  return [Image(file, io.imread(f'Vistex/{file}')) for file in files]
 
 def plot_histogram(histogram: np.array, color: str):
   indexes = np.arange(np.size(histogram))
@@ -67,11 +94,20 @@ if __name__ == "__main__":
   # - Caminho de uma pasta contendo imagens a serem buscadas
   # - Inteiro N que indica o número de imagens parecidas a serem retornadas
 
-  # 1. Abra o zip e coloque as imagens em uma lista
-  imgs = get_images()
+  print("Syntax to use this tool:")
+  print("python image_recovery <search_image_name> <images_path> <number_of_images_to_be_recovered>")
 
-  # 2. Construa histograma de cada imagem
-  histogram = make_histograms(imgs)
-  # 3. Obtenha PDF (Probability Density Function) para cada imagem
+  search_image = sys.argv[1]
+  images_path = sys.argv[2]
+  number_results = sys.argv[3]
+  
+  # Initialize all images with it's histograms and pdfs
+  imgs = initialize_images()
 
-  # 4. Faça a recuperação das imagens usando diferentes métodos
+  print(imgs[0].filename)
+  print(imgs[0].contents)
+  print(imgs[0].filename)
+  print(imgs[0].filename)
+  print(imgs[0].filename)
+
+  # Faça a recuperação das imagens usando diferentes métodos
