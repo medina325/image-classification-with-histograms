87a440c778fbf2bbb3cc7640a6f2f14fff62f9e3
Gabriel Medina Braga <56964034+medina325@users.noreply.github.com>
2021-09-04 00:42:03 -0400

    reformulando criação de histogramas; criando pdfs; mudando abertura de arquivo

--- image_recovery.py
+++ image_recovery.py
@@ -31,32 +31,33 @@ class Image:
   """
     Initialize Image's histograms
   """
-  def create_histogram(self):
-    histogram_r = np.zeros(256, np.uint)
-    histogram_g = np.zeros(256, np.uint)
-    histogram_b = np.zeros(256, np.uint)
-    
-    w, h, _ = self.contents.shape
-
-    for x in range(w):
-      for y in range(h):
-        histogram_r[self.contents[x][y][0]] += 1
-        histogram_g[self.contents[x][y][1]] += 1
-        histogram_b[self.contents[x][y][2]] += 1
-
-    self.histograms['red'] = histogram_r
-    self.histograms['green'] = histogram_g
-    self.histograms['blue'] = histogram_b
+  def create_histogram(self) -> None:
+    self.histograms['red'][self.contents[:, :, 0]] += 1
+    self.histograms['green'][self.contents[:, :, 1]] += 1
+    self.histograms['blue'][self.contents[:, :, 2]] += 1
 
   def create_pdfs(self):
-    histogram_length = self.histogram['red'].shape[0]
-    
-    self.pdf['red'] = self.histogram['red'] / histogram_length
-    self.pdf['green'] = self.histogram['green'] / histogram_length
-    self.pdf['blue'] = self.histogram['blue'] / histogram_length
+    histogram_length = self.histograms['red'].shape[0]
+
+    self.pdf['red'] = self.histograms['red'] / histogram_length
+    self.pdf['green'] = self.histograms['green'] / histogram_length
+    self.pdf['blue'] = self.histograms['blue'] / histogram_length
 
 # --------------------------------------------------------------------------------------------
-def initialize_images():
+def open_images_w_path(path: str):
+  if (not os.path.exists(path)):
+    # raise exception
+    pass
+  
+  return os.listdir(path)
+
+def initialize_images(search_image: str, files: list, path: str) -> list:
+  return [Image(file, io.imread(f'{path}/{file}')) for file in files if file != search_image]
+  
+def initialize_search_image(search_image, path):
+  return Image(search_image, io.imread(f'{path}/{search_image}'))
+
+def extract_images_from_vistex_zip():
   # Abra o zip e coloque as imagens em uma lista
 
   if (not os.path.exists('Vistex')):
@@ -64,16 +65,6 @@ def initialize_images():
       for filename in zip.namelist():
         if re.match(r'Vistex/.+\.png', filename):
           zip.extract(filename)
-    zip.close()
-
-  files = os.listdir('Vistex')
-
-  # imgs = []
-  # for i in range(0,3):
-  #   imgs.append(Image(files[i], io.imread(f'Vistex/{files[i]}')))
-  # return imgs
-
-  return [Image(file, io.imread(f'Vistex/{file}')) for file in files]
 
 def plot_histogram(histogram: np.array, color: str):
   indexes = np.arange(np.size(histogram))
@@ -94,20 +85,26 @@ if __name__ == "__main__":
   # - Caminho de uma pasta contendo imagens a serem buscadas
   # - Inteiro N que indica o número de imagens parecidas a serem retornadas
 
-  print("Syntax to use this tool:")
-  print("python image_recovery <search_image_name> <images_path> <number_of_images_to_be_recovered>")
+  print("Syntax to use this tool:\n")
+  print("python image_recovery <search_image_name> <images_path> <number_of_images_to_be_recovered>", end="\n\n")
 
   search_image = sys.argv[1]
   images_path = sys.argv[2]
   number_results = sys.argv[3]
   
   # Initialize all images with it's histograms and pdfs
-  imgs = initialize_images()
+  try:
+    files = open_images_w_path(images_path)
 
-  print(imgs[0].filename)
-  print(imgs[0].contents)
-  print(imgs[0].filename)
-  print(imgs[0].filename)
-  print(imgs[0].filename)
+    imgs = initialize_images(search_image, files, images_path)
+    search_image = initialize_search_image(search_image, images_path)
+    
+    # Faça a recuperação das imagens usando diferentes métodos
+    # Distância Euclidiana
 
-  # Faça a recuperação das imagens usando diferentes métodos
+    # Chi-quadrado
+
+    # Chi modificado
+
+  except FileNotFoundError as e:
+    print(e)
