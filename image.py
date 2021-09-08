6153baa44cd2f76fc595ac2994e13326b32881c6
Gabriel Medina Braga <56964034+medina325@users.noreply.github.com>
2021-09-08 01:31:18 -0400

    refatorando classe Image, método da distância euclidiana implementado

---
+++ image.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Image:
+  """Represents an image with the properties required for the problem"""
+
+  filename = ''
+  contents = []
+  histograms = {
+    'red': np.zeros(256, np.uint),
+    'green': np.zeros(256, np.uint),
+    'blue': np.zeros(256, np.uint)
+  }
+  pdfs = {
+    'red': np.zeros(256, np.uint),
+    'green': np.zeros(256, np.uint),
+    'blue': np.zeros(256, np.uint)
+  }
+  distances = {
+    'red': 0.0,
+    'green': 0.0,
+    'blue': 0.0
+  }
+
+  def __init__(self, filename: str, contents: list) -> None:
+      self.filename = filename
+      self.contents = np.array(contents, np.uint8)
+
+      self.create_histogram()
+      self.create_pdfs()
+
+  def create_histogram(self, gray=False) -> None:
+    """Create Image's histograms"""
+    
+    if not gray:
+      self.histograms['red'][self.contents[:, :, 0]] += 1
+      self.histograms['green'][self.contents[:, :, 1]] += 1
+      self.histograms['blue'][self.contents[:, :, 2]] += 1
+    else:
+      self.histograms['gray'][self.contents[:, :]] += 1
+
+  def create_pdfs(self) -> None:
+    """Create Image's PDFs (Probability Density Function)"""
+    
+    histogram_length = self.histograms['red'].shape[0]
+
+    self.pdfs['red'] = self.histograms['red'] / histogram_length
+    self.pdfs['green'] = self.histograms['green'] / histogram_length
+    self.pdfs['blue'] = self.histograms['blue'] / histogram_length
+
+  def calc_pdfs_distances(self, search_img_pdfs, approach) -> None:
+    """Calculate Image's PDF's distances to another Image's PDFs"""
+    
+    self.distances['red'] = approach(search_img_pdfs['red'], self.pdfs['red'])
+    self.distances['green'] = approach(search_img_pdfs['green'], self.pdfs['green'])
+    self.distances['blue'] = approach(search_img_pdfs['blue'], self.pdfs['blue'])
+
+  def plot_histogram(histogram: np.array, color: str):
+    indexes = np.arange(np.size(histogram))
+    fig = plt.figure()
+    ax = fig.add_axes([0,0,1,1])
+    ax.bar(indexes, histogram, edgecolor='black', color=color)
+    ax.set_ylabel('Oin')
+    # ax.set_title(f'{color.capitalize()} Channel Histogram')
+    ax.set_title('Oin')
+    # ax.set_xticks(indexes, ('G1', 'G2', 'G3', 'G4'))
+    ax.set_yticks(np.arange(0, 81, 10))
+    # ax.legend(labels=['Men', 'Women'])
+    plt.show()
--- image_recovery.py
+++ image_recovery.py
@@ -1,52 +1,8 @@
 import numpy as np
 from skimage import io
 import os
-from zipfile import ZipFile
-import re
-import matplotlib.pyplot as plt
 import sys
-
-class Image:
-  """Represents an image with the properties required for the problem"""
-
-  filename = ''
-  contents = []
-  histograms = {
-    "red": np.zeros(256, np.uint),
-    "green": np.zeros(256, np.uint),
-    "blue": np.zeros(256, np.uint)
-  }
-  pdfs = {
-    "red": np.zeros(256, np.uint),
-    "green": np.zeros(256, np.uint),
-    "blue": np.zeros(256, np.uint)
-  }
-
-  def __init__(self, filename: str, contents: list) -> None:
-      self.filename = filename
-      self.contents = np.array(contents, np.uint8)
-
-      self.create_histogram()
-      self.create_pdfs()
-
-  def create_histogram(self, gray=False) -> None:
-    """Create Image's histograms"""
-    
-    if not gray:
-      self.histograms['red'][self.contents[:, :, 0]] += 1
-      self.histograms['green'][self.contents[:, :, 1]] += 1
-      self.histograms['blue'][self.contents[:, :, 2]] += 1
-    else:
-      self.histograms['gray'][self.contents[:, :]] += 1
-
-  def create_pdfs(self) -> None:
-    """Create Image's PDFs (Probability Density Function)"""
-    
-    histogram_length = self.histograms['red'].shape[0]
-
-    self.pdfs['red'] = self.histograms['red'] / histogram_length
-    self.pdfs['green'] = self.histograms['green'] / histogram_length
-    self.pdfs['blue'] = self.histograms['blue'] / histogram_length
+from image import Image
 
 # --------------------------------------------------------------------------------------------
 def open_images_w_path(path: str) -> list[str]:
@@ -62,57 +18,36 @@ def initialize_search_image(search_image, path) -> Image:
   return Image(search_image, io.imread(f'{path}/{search_image}'))
 
 # --------------------------------------------------------------------------------------------
-def n_most_similar_imgs(n, imgs_w_distances: dict) -> list[str]:
-  pass
+def n_most_similar_imgs(n: int, imgs_to_be_searched: list[Image]) -> list[str]:
+  return sorted(imgs_to_be_searched, key=lambda x: x.distances['red'])[:n]
 
 def euclidian_distance(pe, pi) -> float:
-  pass
+  return np.sum(np.square(pe - pi))
 
-def most_similar_imgs(approach: str, search_img: Image, imgs_to_be_searched: list[Image], number_results: int) -> list[str]:
-  imgs_w_distances = []
-  
+def most_similar_imgs(approach, search_img: Image, imgs_to_be_searched: list[Image], number_results: int) -> list[str]:  
   for img in imgs_to_be_searched:
-    imgs_w_distances.append({
-      'img': img.filename,
-      'distance': approach(search_img.pdfs, img.pdfs)
-    })
+    img.calc_pdfs_distances(search_img.pdfs, approach)
 
-  return n_most_similar_imgs(number_results, imgs_w_distances)
+  return n_most_similar_imgs(number_results, imgs_to_be_searched)
 
-def extract_images_from_vistex_zip() -> None:
-  """Abra o zip e coloque as imagens em uma lista"""
-
-  if (not os.path.exists('Vistex')):
-    with ZipFile('Vistex.zip', 'r') as zip:
-      for filename in zip.namelist():
-        if re.match(r'Vistex/.+\.png', filename):
-          zip.extract(filename)
-
-def plot_histogram(histogram: np.array, color: str):
-  indexes = np.arange(np.size(histogram))
-  fig = plt.figure()
-  ax = fig.add_axes([0,0,1,1])
-  ax.bar(indexes, histogram, edgecolor='black', color=color)
-  ax.set_ylabel('Oin')
-  # ax.set_title(f'{color.capitalize()} Channel Histogram')
-  ax.set_title('Oin')
-  # ax.set_xticks(indexes, ('G1', 'G2', 'G3', 'G4'))
-  ax.set_yticks(np.arange(0, 81, 10))
-  # ax.legend(labels=['Men', 'Women'])
-  plt.show()
-
+# --------------------------------------------------------------------------------------------
 if __name__ == "__main__":
   # Entradas:
   # - Imagem de busca
   # - Caminho de uma pasta contendo imagens a serem buscadas
-  # - Inteiro N que indica o número de imagens parecidas a serem retornadas
+  # - Inteiro n que indica o número de imagens parecidas a serem retornadas
 
   print("Syntax to use this tool:\n")
-  print("python image_recovery <images_path> <search_image_name> <number_of_images_to_be_recovered>", end="\n\n")
+  print("python image_recovery.py <images_path> <search_image_name> <number_of_images_to_be_recovered>", end="\n\n")
 
-  images_path = sys.argv[1]
-  search_image = sys.argv[2]
-  number_results = sys.argv[3]
+  if (len(sys.argv) > 1):
+    images_path = sys.argv[1]
+    search_image = sys.argv[2]
+    number_results = int(sys.argv[3])
+  else:
+    images_path = input("Digite caminho das imagens:\n")
+    search_image = input("Digite nome da imagem a ser buscada:\n")
+    number_results = int(input("Digite número de imagens semelhantes a serem retornadas:\n"))
   
   try:
     # Initialize all images with it's histograms and pdfs
@@ -123,18 +58,18 @@ if __name__ == "__main__":
     
     # Faça a recuperação das imagens usando diferentes métodos
     # Distância Euclidiana
-    ed_similar_imgs = most_similar_imgs('ed', search_image, imgs_to_be_searched, number_results)
-
+    ed_similar_imgs = most_similar_imgs(euclidian_distance, search_image, imgs_to_be_searched, number_results)
+    
     # Chi-quadrado
-    s_chi_similar_imgs = most_similar_imgs('square_chi', search_image, imgs_to_be_searched, number_results)
+    # s_chi_similar_imgs = most_similar_imgs('square_chi', search_image, imgs_to_be_searched, number_results)
 
     # Chi modificado
-    mod_chi_similar_imgs = most_similar_imgs('mod_chi', search_image, imgs_to_be_searched, number_results)
+    # mod_chi_similar_imgs = most_similar_imgs('mod_chi', search_image, imgs_to_be_searched, number_results)
 
+    # TODO
     # Avaliar resultados (porcentagem de classificação correta)
-    
-
-
+    filenames = [img.filename for img in ed_similar_imgs]
+    print(filenames)
 
   except FileNotFoundError as e:
     print(e)
